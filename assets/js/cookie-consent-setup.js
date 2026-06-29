1b8c2579ef7701c2c29df0724a346f471bcb9d71
yankee624 <35090493+yankee624@users.noreply.github.com>
2026-06-29 11:05:10 +0000

    Deploying to gh-pages from @ yankee624/yankee624.github.io@fe2601b91838880ad0b572a99c08808cf5c1d6d4 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://yankee624.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://kichang.me/#contact">contact us</a>.'
               }
             ]
           }
